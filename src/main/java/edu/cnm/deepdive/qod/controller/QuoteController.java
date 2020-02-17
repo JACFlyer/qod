5b1e860500115660b142c39eb9ff098248a73b30
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-02-17 14:53:44 -0700

    Many to One entity changes

--- src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
@@ -18,12 +18,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("quotes")
+@RequestMapping("/quotes")
 
 public class QuoteController {
 
@@ -52,16 +53,24 @@ public class QuoteController {
 
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public Iterable<Quote> get() {
+  public Iterable<Quote> get(String fragment) {
     return quoteRepository.getAllByOrderByCreatedDesc();
   }
 
-  @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Quote> search(@RequestParam("q") String fragment) {
+    return quoteRepository.getAllByTextContainsOrderByTextAsc(fragment);
+  }
+
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote get(@PathVariable UUID id) {
     return quoteRepository.findById(id).get();
   }
 
-  @PutMapping(value = "{id}",
+
+
+  @PutMapping(value = "/{id}",
       consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote put(@PathVariable UUID id, @RequestBody Quote modifiedQuote) {
     Quote quote = get(id);
@@ -69,7 +78,7 @@ public class QuoteController {
     return quoteRepository.save(quote);
   }
 
-  @PutMapping(value = "{id}/text", consumes = MediaType.TEXT_PLAIN_VALUE,
+  @PutMapping(value = "/{id}/text", consumes = MediaType.TEXT_PLAIN_VALUE,
       produces = MediaType.TEXT_PLAIN_VALUE)
   public String put(@PathVariable UUID id, @RequestBody String modifiedQuote) {
     Quote quote = get(id);
@@ -79,7 +88,7 @@ public class QuoteController {
   }
 
 
-  @DeleteMapping(value = "{id}")
+  @DeleteMapping(value = "/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable UUID id) {
     // Code below throws NoSuchElelmentException if id is not in database
@@ -91,25 +100,31 @@ public class QuoteController {
 
   }
 
-  @PutMapping(value = "{quoteId}/sources/{sourceId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PutMapping(value = "/{quoteId}/source/{sourceId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote attach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
     Quote quote = get(quoteId);
     Source source = sourceRepository.findById(sourceId).get();
-    if (quote.getSources().add(source)) {
+    if (!source.equals(quote.getSource())) {
+      quote.setSource(source);
       quoteRepository.save(quote);
     }
     return quote;
   }
 
-
-
-  @DeleteMapping(value = "{quoteId}/sources/{sourceId}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void detach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
+  @DeleteMapping(value = "/{quoteId}/source/{sourceId}")
+  public Quote detach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
     Quote quote = get(quoteId);
     Source source = sourceRepository.findById(sourceId).get();
-    if (quote.getSources().remove(source)) {
+    if (source.equals(quote.getSource())) {
+      quote.setSource(null);
       quoteRepository.save(quote);
     }
+    return quote;
+  }
+  @DeleteMapping(value = "/{quoteId}/source")
+  public Quote clearSource(@PathVariable UUID quoteId) {
+    Quote quote = get(quoteId);
+    quote.setSource(null);
+    return quoteRepository.save(quote);
   }
 }
--- src/main/java/edu/cnm/deepdive/qod/model/entity/Quote.java
+++ src/main/java/edu/cnm/deepdive/qod/model/entity/Quote.java
@@ -1,10 +1,10 @@
 package edu.cnm.deepdive.qod.model.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import edu.cnm.deepdive.qod.view.FlatQuote;
+import edu.cnm.deepdive.qod.view.FlatSource;
 import java.util.Date;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.Objects;
 import java.util.UUID;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,9 +14,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OrderBy;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -28,10 +27,12 @@ import org.springframework.lang.NonNull;
 @Entity
 @Table(
     indexes = {
-        @Index(columnList = "created")
+    @Index(columnList = "created"),
+    @Index(columnList = "text"),
+    @Index(columnList = "source_id, text", unique = true)
     }
 )
-public class Quote {
+public class Quote implements FlatQuote {
 
   @NonNull
   @Id
@@ -58,46 +59,33 @@ public class Quote {
 
 
   @NonNull
-  @Column(length = 4096, nullable = false, unique = true)
+  @Column(length = 4096, nullable = false)
   private String text;
 
 
-  // Many to Many Entity code
-  @NonNull
-  @ManyToMany(fetch = FetchType.LAZY,
-      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
-  )
-  @JoinTable(
-      joinColumns = {
-          @JoinColumn(name = "quote_id")
-      },
-      inverseJoinColumns = {
-          @JoinColumn(name = "source_id")
-      }
-  )
-
-  @OrderBy("name ASC")
-  private Set<Source> sources = new LinkedHashSet<>();
-
-  // Many to Many code end
+  @ManyToOne(fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinColumn(name = "source_id")
+  @JsonSerialize(as = FlatSource.class)
+  private Source source;
 
 
-  @NonNull
+  @Override
   public UUID getId() {
     return id;
   }
 
-  @NonNull
+  @Override
   public Date getCreated() {
     return created;
   }
 
-  @NonNull
+  @Override
   public Date getUpdated() {
     return updated;
   }
 
-  @NonNull
+  @Override
   public String getText() {
     return text;
   }
@@ -106,9 +94,32 @@ public class Quote {
     this.text = text;
   }
 
-  @NonNull
-  public Set<Source> getSources()
-  {
-    return sources;
+  public Source getSource() {
+    return source;
   }
-}
+
+  public void setSource(Source source) {
+    this.source = source;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, text);  // TODO Compute lazily, and cache.
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean result = false;
+      if (obj == this) {
+        result = true;
+      } else if (obj instanceof Quote && obj.hashCode() == hashCode()) {
+        Quote other = (Quote) obj;
+        result = id.equals(other.id) && text.equals(other.text);
+      }
+      return result;
+
+
+    }
+    }
+
+
